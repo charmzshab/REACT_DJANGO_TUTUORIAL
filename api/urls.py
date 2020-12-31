e77eacf1a123f4f0ab546c22a64df67346d30d13
Shabix <kmharouna@gmail.com>
2020-12-31 11:04:34 +0300

    Added logic for handling api POST requests

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,7 @@ from django.urls import path,include
 from .  import views
 
 urlpatterns = [
-    path('room', views.RoomView.as_view(),name="home"),
+    path('room/', views.RoomView.as_view(),name="room"),
+    path('create-room/', views.CreateRoomView.as_view(),name="create-room")
 
 ]    
