dffd3428703732c76199bfb11b304aacb18de72d
Shabix <kmharouna@gmail.com>
2021-01-01 12:42:15 +0300

    Added logic for ComponentDidMount on the HomePage and Django sessions in the UserInRoom view function

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -2,19 +2,61 @@
  import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
 import Room from "./Room";
+
+import { Button, Grid, Typography, ButtonGroup} from '@material-ui/core';
+
 import { BrowserRouter as Router, Switch, Route, Link,Redirect } from "react-router-dom"
 
 
  export default class HomePage extends Component {
      constructor(props){
          super(props);
+         this.state = {
+             roomCode: null,
+         };
+     }
+
+     async componentDidMount(){ //means the component just did mount for the first time on the screen(its a life cycle method)
+        fetch("/api/user-in-room")
+            .then((response) => response.json())
+            .then((data) => {
+                this.setState({
+                    roomCode: data.code
+                });
+            });
+     }
+
+     renderHomePage(){
+         return (
+            <Grid container spacing={3}>
+                <Grid item xs={12} align="center">
+                    <Typography variant="h3" component="h3">
+                        House Party
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <ButtonGroup disableElevation variant="contained" color="primary">
+                        <Button color="primary" to='/join' component={ Link }>
+                            Join a Room
+                        </Button>
+                        <Button color="secondary" to='/create' component={ Link }>
+                            Create a Room
+                        </Button>
+                    </ButtonGroup>
+                </Grid>
+
+            </Grid>
+         );
+
      }
 
      render(){
          return (
              <Router>
                  <switch>
-                     <Route exact path="/"><p>This is the home page</p></Route>
+                     <Route exact path="/">{this.renderHomePage()}</Route>
+                     <Route exact path="/" render={() =>{return this.state.roomCode ? 
+                        (<Redirect to={`/room/${this.state.roomCode}`} />):(this.renderHomePage())} }/>
                      <Route path='/join' component={RoomJoinPage} />
                      <Route path='/create' component={CreateRoomPage} />
                      <Route path='/room/:roomCode' component={Room} />
--- frontend/src/components/Room.js
+++ frontend/src/components/Room.js
@@ -30,7 +30,7 @@ export default class Room extends Component{
             <div>
                 <h3>{this.roomCode}</h3>
                 <p>Votes:{this.state.votesToSkip}</p>
-                <p>Guest:{this.state.guestCanPause.toString()}</p>
+                <p>Guest Can Pause:{this.state.guestCanPause.toString()}</p>
                 <p>Host:{this.state.isHost.toString()}</p>
             </div>
         );
